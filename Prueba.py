40f04f85228467cf191eb2037b9896b8dd19fc7c
Juan <jmartinezsb2020@gmail.com>
2021-05-11 21:40:57 -0500

    Multilane model animation fixed

--- Prueba.py
+++ Prueba.py
@@ -1,5 +1,17 @@
-lista = []
+import random
 
+lista = []
 lista += [None] * 5
 
-print(lista)
+lista[0] = 2
+lista[2] = 3
+lista[4] = 1
+
+pos = 0
+pos1 = 2
+
+if(pos + 5 == 5):
+    print(pos,' $ ', pos1)
+
+
+
--- Road.py
+++ Road.py
@@ -1,3 +1,5 @@
+import random
+
 class Road ():
 
     def __init__(self):
@@ -11,59 +13,55 @@ class Road ():
 
     def singleLaneUpdate(self):
         lane = self.lanes[0]
-        for vehicle in lane.vehicleList:
-            if(vehicle != None):
-                if(not(vehicle.checked)):
-                    vehicle.singleLaneUpdatePosition(lane.checkGap(vehicle),lane.maxSpeed)
-                    lane.vehicleList[vehicle.currentPos] = None
-                    if(vehicle.newPos < len(lane.vehicleList)):
-                        lane.vehicleList[vehicle.newPos] = vehicle
-                        vehicle.currentPos = vehicle.newPos
-                        vehicle.checked = True
-                    else:
-                        lane.occupiedCells -=1
-                        # nameIndex = lane.vehicleNames.index(lane.__getVehicleNameIndex(vehicle.name))
-                        # lane.vehicleNames[nameIndex][1] = False
-                        vehicle.kill()
+        for vehicle in list(filter(None, lane.vehicleList)):
+            if(not(vehicle.checked)):
+                vehicle.singleLaneUpdatePosition(lane.checkGap(vehicle),lane.maxSpeed)
+                lane.vehicleList[vehicle.currentPos] = None
+                if(vehicle.newPos < len(lane.vehicleList)):
+                    lane.vehicleList[vehicle.newPos] = vehicle
+                    vehicle.currentPos = vehicle.newPos
+                    vehicle.checked = True
+                else:
+                    lane.occupiedCells -=1
+                    vehicle.kill()
         lane.restartVehicleValues()
 
     def multiLaneUpdate(self):
         for lane in self.lanes:
-            for vehicle in lane.vehicleList:
-                if(vehicle != None):
-                    if(not(vehicle.checked)):
-                        ############ Data for second rule ###########
-                        movement = self.validateSideMovement(vehicle) ## MOVE UP OR DOWN ##
-                        gaps = []
-                        if(movement == 'UP'):
-                            gaps = [self.forwardGap(self.lanes[vehicle.lane - 1], vehicle.currentPos), self.backwardGap(lane, vehicle.currentPos)]
-                        elif(movement == 'DOWN'):
-                            gaps = [self.forwardGap(self.lanes[vehicle.lane + 1], vehicle.currentPos), self.backwardGap(lane, vehicle.currentPos)]
-                        #####################################################################
-                        currentLane = vehicle.lane ## Carril actual ###
-                        vehicle.multiLaneUpdatePosition(lane.checkGap(vehicle), gaps,lane.maxSpeed, movement)
-                        if(vehicle.lane == currentLane):
-                            movement = 'N/A'
-                        lane.vehicleList[vehicle.currentPos] = None
-                        vehicle.checked = True
-                        if(movement != 'N/A'):
-                            lane.occupiedCells -=1
-                            self._changeLanesVehicle(vehicle)
+            for vehicle in list(filter(None, lane.vehicleList)):
+                if(not(vehicle.checked)):
+                    ############ Data for second rule ###########
+                    movement = self.validateSideMovement(vehicle) ## MOVE UP OR DOWN ##
+                    gaps = []
+                    if(movement == 'UP'):
+                        gaps = [self.backwardGap(lane, vehicle.currentPos), self.forwardGap(self.lanes[vehicle.lane - 1], vehicle.currentPos)]
+                    elif(movement == 'DOWN'):
+                        gaps = [self.backwardGap(lane, vehicle.currentPos), self.forwardGap(self.lanes[vehicle.lane + 1], vehicle.currentPos)]
+                    #####################################################################
+                    currentLane = vehicle.lane ## Carril actual ###
+                    gap = lane.checkGap(vehicle)
+                    vehicle.multiLaneUpdatePosition(gap, gaps,lane.maxSpeed, movement)
+                    if(vehicle.lane == currentLane):
+                        movement = 'N/A'
+                    lane.vehicleList[vehicle.currentPos] = None
+                    vehicle.checked = True
+                    if(movement != 'N/A'):
+                        lane.occupiedCells -=1
+                        self._changeLanesVehicle(vehicle)
+                    else:
+                        if(vehicle.newPos < len(lane.vehicleList)):
+                            lane.vehicleList[vehicle.newPos] = vehicle
+                            vehicle.currentPos = vehicle.newPos
                         else:
-                            if(vehicle.newPos < len(lane.vehicleList)):
-                                lane.vehicleList[vehicle.newPos] = vehicle
-                                vehicle.currentPos = vehicle.newPos
-                            else:
-                                lane.occupiedCells -=1
-                                # nameIndex = lane.vehicleNames.index(lane.__getVehicleNameIndex(vehicle.name))
-                                # lane.vehicleNames[nameIndex][1] = False
-                                vehicle.kill()
+                            lane.occupiedCells -=1
+                            vehicle.kill()
             lane.restartVehicleValues()
 
     def _changeLanesVehicle(self, vehicle):
-        lane = self.lanes[vehicle.lane]
-        lane.vehicleList[vehicle.currentPos] = vehicle
         vehicle.kill()
+        lane = self.lanes[vehicle.lane]
+        vehicle.currentPos = vehicle.newPos ## Actualiza la posicion al nuevo carril ##
+        lane.vehicleList[vehicle.currentPos] = vehicle
         lane.add(vehicle)
         lane.occupiedCells += 1
 
@@ -71,32 +69,40 @@ class Road ():
         moveUp = (vehicle.lane - 1) >= 0
         moveDown = (vehicle.lane + 1) < len(self.lanes)
         #######################################################
+        moves = []
         if(moveUp):
             laneOne = self.lanes[vehicle.lane - 1] ## UP
             if(laneOne.vehicleList[vehicle.currentPos] == None):
-                return 'UP'
+                moves.append('UP')
         if(moveDown):
             laneTwo = self.lanes[vehicle.lane + 1] ## DOWN
             if(laneTwo.vehicleList[vehicle.currentPos] == None):
-                return 'DOWN'        
-        return 'N/A'
+                moves.append('DOWN')
+        if(len(moves) == 2):
+            return moves[random.randint(0,1)]
+        elif(len(moves) == 1): 
+            return moves[0]
+        else:
+            return 'N/A'
 
     def forwardGap(self, lane, pos):
         gap = 0
-        for i in range((pos + 1), len(lane.vehicleList)): ## 
-            vehicle = lane.vehicleList[i]
-            if(vehicle == None):
-                gap+=1
-            else:
-                return gap
+        if(pos > 0):
+            for i in range((pos + 1), len(lane.vehicleList)):
+                vehicle = lane.vehicleList[i]
+                if(vehicle == None):
+                    gap += 1
+                else:
+                    return gap  
         return gap
         
     def backwardGap(self, lane, pos):
         gap = 0
-        for i in range((pos - 1), 0, -1): ## 
-            vehicle = lane.vehicleList[i]
-            if(vehicle == None):
-                gap += 1
-            else:
-                return gap
+        if(pos > 0):
+            for i in range((pos - 1), 0, -1): ## 
+                vehicle = lane.vehicleList[i]
+                if(vehicle == None):
+                    gap += 1
+                else:
+                    return gap
         return gap
